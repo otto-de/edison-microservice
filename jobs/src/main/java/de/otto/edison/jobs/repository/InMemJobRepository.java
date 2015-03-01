9c0a80c71014c3adb6078bcc5907b1a5ae758a35
Guido Steinacker <guido.steinacker@otto.de>
2015-03-01 14:16:05 +0100

    Added JobRepository.size() and JobRepository.deleteOldest()

--- jobs/src/main/java/de/otto/edison/jobs/repository/InMemJobRepository.java
+++ jobs/src/main/java/de/otto/edison/jobs/repository/InMemJobRepository.java
@@ -1,15 +1,19 @@
 package de.otto.edison.jobs.repository;
 
 import de.otto.edison.jobs.domain.JobInfo;
+import de.otto.edison.jobs.domain.JobType;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.Predicate;
 
 import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
 import static java.util.Comparator.reverseOrder;
 import static java.util.Optional.ofNullable;
 
@@ -30,8 +34,40 @@ public class InMemJobRepository implements JobRepository {
     @Override
     public List<JobInfo> findAll() {
         final List<JobInfo> values = new ArrayList<>(jobs.values());
-        values.sort(comparing(JobInfo::getStarted, reverseOrder()));
+        values.sort(youngestJobFirst());
         return values;
+    }
+
+    private Comparator<JobInfo> youngestJobFirst() {
+        return comparing(JobInfo::getStarted, reverseOrder());
+    }
+
+    @Override
+    public int size() {
+        return jobs.size();
+    }
+
+    @Override
+    public void deleteOldest(final Optional<JobType> jobType) {
+        jobs.values().stream()
+                .sorted(oldestJobFirst())
+                .filter(jobInfoMatchesTo(jobType))
+                .findFirst()
+                .ifPresent(
+                        jobInfo -> jobs.remove(jobInfo.getJobUri())
+                );
+    }
+
+    private Comparator<JobInfo> oldestJobFirst() {
+        return comparing(JobInfo::getStarted, naturalOrder());
+    }
+
+    private Predicate<JobInfo> jobInfoMatchesTo(Optional<JobType> jobType) {
+        if (jobType.isPresent()) {
+            return jobInfo -> (jobInfo.getJobType().equals(jobType.get()) && jobInfo.getStopped().isPresent());
+        } else {
+            return jobInfo -> jobInfo.getStopped().isPresent();
+        }
     }
 
 }
---
+++ jobs/src/test/java/de/otto/edison/jobs/repository/InMemJobRepositoryTest.java
@@ -0,0 +1,95 @@
+package de.otto.edison.jobs.repository;
+
+import de.otto.edison.jobs.domain.JobInfo;
+import de.otto.edison.jobs.domain.JobType;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+
+import static de.otto.edison.jobs.domain.JobInfoBuilder.jobInfoBuilder;
+import static de.otto.edison.testsupport.matcher.OptionalMatchers.isPresent;
+import static java.time.LocalDateTime.now;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class InMemJobRepositoryTest {
+
+    @Test
+    public void shouldNotDeleteRunningJobs() {
+        // given
+        InMemJobRepository repository = new InMemJobRepository();
+        JobType jobType = () -> "FOO";
+        repository.createOrUpdate(
+                jobInfoBuilder(jobType, URI.create("test")).build()
+        );
+        // when
+        repository.deleteOldest(of(jobType));
+        // then
+        assertThat(repository.size(), is(1));
+    }
+
+    @Test
+    public void shouldNotFailToDeleteMissingJob() {
+        // given
+        InMemJobRepository repository = new InMemJobRepository();
+        // when
+        repository.deleteOldest(of((JobType) () -> "FOO"));
+        repository.deleteOldest(empty());
+        // then
+        // no Exception is thrown...
+    }
+
+    @Test
+    public void shouldDeleteStoppedJob() {
+        // given
+        InMemJobRepository repository = new InMemJobRepository();
+        JobType jobType = () -> "FOO";
+        repository.createOrUpdate(
+                jobInfoBuilder(jobType, URI.create("test")).withStopped(now()).build()
+        );
+        // when
+        repository.deleteOldest(of(jobType));
+        // then
+        assertThat(repository.size(), is(0));
+    }
+
+    @Test
+    public void shouldDeleteOldestStoppedJobOfType() {
+        // given
+        InMemJobRepository repository = new InMemJobRepository();
+        JobType jobType = () -> "FOO";
+        repository.createOrUpdate(
+                jobInfoBuilder(jobType, URI.create("42")).withStarted(now().minusSeconds(1)).withStopped(now()).build()
+        );
+        JobInfo expectedSurvivor = jobInfoBuilder(jobType, URI.create("0815")).withStopped(now()).build();
+        repository.createOrUpdate(
+                expectedSurvivor
+        );
+        // when
+        repository.deleteOldest(of(jobType));
+        // then
+        assertThat(repository.size(), is(1));
+        assertThat(repository.findBy(expectedSurvivor.getJobUri()), isPresent());
+    }
+
+    @Test
+    public void shouldDeleteOldestStoppedJob() {
+        // given
+        InMemJobRepository repository = new InMemJobRepository();
+        repository.createOrUpdate(
+                jobInfoBuilder(() -> "FOO", URI.create("42")).withStarted(now().minusSeconds(1)).withStopped(now()).build()
+        );
+        JobInfo expectedSurvivor = jobInfoBuilder(() -> "BAR", URI.create("0815")).withStopped(now()).build();
+        repository.createOrUpdate(
+                expectedSurvivor
+        );
+        // when
+        repository.deleteOldest(empty());
+        // then
+        assertThat(repository.size(), is(1));
+        assertThat(repository.findBy(expectedSurvivor.getJobUri()), isPresent());
+    }
+
+}
