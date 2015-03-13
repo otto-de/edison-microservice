f6f1b3ca540209ee0411f6c458b8fd916d5a04a4
Florian Sellmayr <florian.sellmayr@otto.de>
2015-03-13 12:56:50 +0100

    missing component annotation

--- jobs/src/main/java/de/otto/edison/jobs/service/Clock.java
+++ jobs/src/main/java/de/otto/edison/jobs/service/Clock.java
@@ -1,7 +1,10 @@
 package de.otto.edison.jobs.service;
 
+import org.springframework.stereotype.Component;
+
 import java.time.OffsetDateTime;
 
+@Component
 public class Clock {
     public OffsetDateTime now() {
         return OffsetDateTime.now();
