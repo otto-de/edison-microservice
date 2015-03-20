3418cfa55326e9975a09925e1aec9f7615bf7384
Daniel Rolfes <daniel.rolfes@ottogroup.com>
2015-03-20 13:58:16 +0100

    add new DEAD status for dead jobs and make the behavior more visible

--- jobs/src/main/java/de/otto/edison/jobs/repository/StopDeadJobs.java
+++ jobs/src/main/java/de/otto/edison/jobs/repository/StopDeadJobs.java
@@ -2,6 +2,7 @@ package de.otto.edison.jobs.repository;
 
 import de.otto.edison.jobs.domain.JobInfo;
 import de.otto.edison.jobs.domain.JobInfoBuilder;
+import de.otto.edison.jobs.service.Clock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static de.otto.edison.jobs.domain.JobInfo.JobStatus.DEAD;
 import static java.lang.String.format;
 
 public class StopDeadJobs implements JobCleanupStrategy {
@@ -16,22 +18,31 @@ public class StopDeadJobs implements JobCleanupStrategy {
     private static final Logger LOG = LoggerFactory.getLogger(StopDeadJobs.class);
 
     private final int stopJobAfterSeconds;
+    private final Clock clock;
 
-    public StopDeadJobs(final int stopJobAfterSeconds) {
+    public StopDeadJobs(final int stopJobAfterSeconds, Clock clock) {
         this.stopJobAfterSeconds = stopJobAfterSeconds;
+        this.clock = clock;
         LOG.info(format("Mark old as stopped after %s seconds of inactivity.", stopJobAfterSeconds));
     }
 
     @Override
     public void doCleanUp(JobRepository repository) {
-        OffsetDateTime timeToMarkJobAsStopped = OffsetDateTime.now().minusSeconds(stopJobAfterSeconds);
+        OffsetDateTime now = clock.now();
+        OffsetDateTime timeToMarkJobAsStopped = now.minusSeconds(stopJobAfterSeconds);
         List<JobInfo> deadJobs = repository.findAll()
                 .stream()
                 .filter(job -> (!job.getStopped().isPresent() && job.getLastUpdated().isBefore(timeToMarkJobAsStopped)))
                 .collect(Collectors.toList());
 
         for (JobInfo deadJob : deadJobs) {
-            JobInfo jobInfo = JobInfoBuilder.copyOf(deadJob).withStopped(deadJob.getLastUpdated()).build();
+            LOG.info("Marking job as dead: {}",deadJob);
+            JobInfo jobInfo = JobInfoBuilder
+                    .copyOf(deadJob)
+                    .withStopped(now)
+                    .withLastUpdated(now)
+                    .withStatus(DEAD)
+                    .build();
             repository.createOrUpdate(jobInfo);
         }
 
