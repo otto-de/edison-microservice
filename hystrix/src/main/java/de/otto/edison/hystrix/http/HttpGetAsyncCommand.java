a934191ef24b8a3c7edbd1753b4d4b9fa82d8fbc
Guido Steinacker <guido.steinacker@otto.de>
2015-04-16 16:42:27 +0200

    Added hystrix module

---
+++ hystrix/src/main/java/de/otto/edison/hystrix/http/HttpGetAsyncCommand.java
@@ -0,0 +1,45 @@
+package de.otto.edison.hystrix.http;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.ning.http.client.AsyncHandler;
+import com.ning.http.client.AsyncHttpClient;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * A Hystrix command that is asynchronously getting a resource using AsyncHttpClient.
+ *
+ * @author Guido Steinacker
+ * @since 15.04.15
+ */
+public final class HttpGetAsyncCommand<T> extends HystrixCommand<T> {
+
+    private final AsyncHttpClient.BoundRequestBuilder requestBuilder;
+    private final AsyncHandler<T> asyncHandler;
+    private final int timeout;
+    private final TimeUnit timeUnit;
+
+
+    public HttpGetAsyncCommand(final HystrixCommandGroupKey commandGroup,
+                               final AsyncHandler<T> asyncHandler,
+                               final AsyncHttpClient.BoundRequestBuilder requestBuilder,
+                               final int timeout,
+                               final TimeUnit timeUnit) {
+        super(commandGroup);
+        this.requestBuilder = requestBuilder;
+        this.asyncHandler = asyncHandler;
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
+    }
+
+    @Override
+    protected T run() throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        return requestBuilder.execute(asyncHandler).get(timeout, timeUnit);
+    }
+
+
+}
