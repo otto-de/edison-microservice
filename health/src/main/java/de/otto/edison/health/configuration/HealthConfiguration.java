eff10e9ae26404905924fffa5d2bb6b022eba3cf
Daniel Rolfes <daniel.rolfes@otto.de>
2015-04-29 14:41:27 +0200

    fh|switch lifecycle status indicator to health indicator

--- health/src/main/java/de/otto/edison/health/configuration/HealthConfiguration.java
+++ health/src/main/java/de/otto/edison/health/configuration/HealthConfiguration.java
@@ -1,11 +1,26 @@
 package de.otto.edison.health.configuration;
 
 import de.otto.edison.health.indicator.ApplicationHealthIndicator;
+import de.otto.edison.health.indicator.GracefulShutdownHealthIndicator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HealthConfiguration {
+
+    @Value("${edison.graceful.shutdown.time.beforeIndicateError:5000}")
+    private long timeBeforeIndicateError;
+
+    @Value("${edison.graceful.shutdown.time.phaseOut:25000}")
+    private long timeForPhaseOut;
+
+    @Bean
+    @ConditionalOnProperty(name = "edison.graceful.shutdown.active", havingValue = "true")
+    public GracefulShutdownHealthIndicator gracefulShutdownHealthIndicator() {
+        return new GracefulShutdownHealthIndicator(timeBeforeIndicateError, timeForPhaseOut);
+    }
 
     @Bean
     public ApplicationHealthIndicator healthIndicator() {
---
+++ health/src/main/java/de/otto/edison/health/indicator/GracefulShutdownHealthIndicator.java
@@ -0,0 +1,84 @@
+package de.otto.edison.health.indicator;
+
+import net.jcip.annotations.ThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.SmartLifecycle;
+
+import static org.springframework.boot.actuate.health.Health.down;
+import static org.springframework.boot.actuate.health.Health.up;
+
+@ThreadSafe
+public class GracefulShutdownHealthIndicator implements SmartLifecycle, HealthIndicator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GracefulShutdownHealthIndicator.class);
+
+    private final long timeBeforeIndicateError;
+    private final long timeForPhaseOut;
+
+    private Health health = up().build();
+
+    public GracefulShutdownHealthIndicator(final long timeBeforeIndicateError, final long timeForPhaseOut) {
+        this.timeBeforeIndicateError = timeBeforeIndicateError;
+        this.timeForPhaseOut = timeForPhaseOut;
+    }
+
+    @Override
+    public Health health() {
+        return health;
+    }
+
+    @Override
+    public void stop(Runnable callback) {
+        try {
+            waitForSettingHealthCheckToDown();
+
+            LOG.info("set health check to down");
+            indicateDown();
+
+            waitForShutdown();
+        } catch (InterruptedException e) {
+            LOG.error("graceful shutdown interrupted", e);
+        }
+        callback.run();
+    }
+
+    private void indicateDown() {
+        health = down().build();
+    }
+
+    void waitForSettingHealthCheckToDown() throws InterruptedException {
+        LOG.info("shutdown signal received ...");
+        Thread.sleep(timeBeforeIndicateError);
+    }
+
+    void waitForShutdown() throws InterruptedException {
+        Thread.sleep(timeForPhaseOut);
+        LOG.info("grace period ended, starting shutdown now");
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public boolean isRunning() {
+        return true;
+    }
+
+    @Override
+    public int getPhase() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+}
