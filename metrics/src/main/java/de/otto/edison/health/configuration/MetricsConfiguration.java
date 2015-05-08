cf25ab7776036368f0e482b6e6129bd24978a96c
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-05-08 13:36:45 +0200

    harmonize metrics versions

--- metrics/src/main/java/de/otto/edison/health/configuration/MetricsConfiguration.java
+++ metrics/src/main/java/de/otto/edison/health/configuration/MetricsConfiguration.java
@@ -2,6 +2,8 @@ package de.otto.edison.health.configuration;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Slf4jReporter;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 import static com.codahale.metrics.Slf4jReporter.LoggingLevel.INFO;
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
@@ -29,6 +30,9 @@ public class MetricsConfiguration extends MetricsConfigurerAdapter {
                 .withLoggingLevel(INFO)
                 .build()
                 .start(1, MINUTES);
+
+        metricRegistry.register("gc", new GarbageCollectorMetricSet());
+        metricRegistry.register("memory", new MemoryUsageGaugeSet());
     }
 
 }
