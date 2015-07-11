06a8156071afa7bdcf8c640259eb14081843d17b
Guido Steinacker <guido.steinacker@otto.de>
2015-07-11 13:00:53 +0200

    Added some logging

--- jobs/src/main/java/de/otto/edison/jobs/service/DefaultJobService.java
+++ jobs/src/main/java/de/otto/edison/jobs/service/DefaultJobService.java
@@ -7,13 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.net.URI;
 import java.time.Clock;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
 
 import static de.otto.edison.jobs.domain.JobInfoBuilder.jobInfoBuilder;
 import static de.otto.edison.jobs.service.JobRunner.createAndPersistJobRunner;
@@ -62,6 +65,11 @@ public class DefaultJobService implements JobService {
         this.gaugeService = gaugeService;
         this.clock = clock;
         this.executor = executor;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("Found {} JobRunnables: {}", + jobRunnables.size(), jobRunnables.stream().map(JobRunnable::getJobType).collect(Collectors.toList()));
     }
 
     @Override
