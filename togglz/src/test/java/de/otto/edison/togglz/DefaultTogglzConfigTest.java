fc3443913d1afac17cf6988e9401d15ee633666e
Bastian Voigt <lhotse-team-ft3@ov.otto.de>
2015-07-14 10:37:55 +0200

    ab/bv Fix the link to jobs on the admin page. Added caching for the togglez querying

--- togglz/src/test/java/de/otto/edison/togglz/DefaultTogglzConfigTest.java
+++ togglz/src/test/java/de/otto/edison/togglz/DefaultTogglzConfigTest.java
@@ -1,12 +1,14 @@
 package de.otto.edison.togglz;
 
 import de.otto.edison.testsupport.applicationdriver.SpringTestBase;
-import de.otto.edison.togglz.configuration.TogglzConfiguration;
+import de.otto.edison.togglz.configuration.InMemoryFeatureStateRepositoryConfiguration;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.togglz.core.manager.TogglzConfig;
+import org.togglz.core.repository.cache.CachingStateRepository;
 
 import static de.otto.edison.testsupport.dsl.Then.assertThat;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.typeCompatibleWith;
@@ -25,13 +27,14 @@ public class DefaultTogglzConfigTest extends SpringTestBase {
     @Test
     public void shouldCreateTogglzConfigBySpring() {
         assertThat(togglzConfig, is(not(nullValue())));
-        assertThat(togglzConfig.getFeatureClass(), typeCompatibleWith(TogglzConfiguration.Features.class));
+        assertThat(togglzConfig.getFeatureClass(), typeCompatibleWith(InMemoryFeatureStateRepositoryConfiguration.Features.class));
         assertThat(togglzConfig.getStateRepository(),is(not(nullValue())));
+        assertThat(togglzConfig.getStateRepository(),is(instanceOf(CachingStateRepository.class)));
         assertThat(togglzConfig.getUserProvider(),is(not(nullValue())));
     }
 
     @Test
     public void shouldProvideToggleStateWhichIsActiveByDefaultInTests() {
-        assertThat(TogglzConfiguration.Features.TEST.isActive(),is(true));
+        assertThat(InMemoryFeatureStateRepositoryConfiguration.Features.TEST.isActive(),is(true));
     }
 }
