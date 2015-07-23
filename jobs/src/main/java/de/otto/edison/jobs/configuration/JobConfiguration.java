73e0664c8a3b1dc263f785cc2648175fc21f435b
Guido Steinacker <guido.steinacker@otto.de>
2015-07-23 16:52:34 +0200

    Fixed broken classpath in IntelliJ for provided dependencies

--- jobs/src/main/java/de/otto/edison/jobs/configuration/JobConfiguration.java
+++ jobs/src/main/java/de/otto/edison/jobs/configuration/JobConfiguration.java
@@ -1,23 +1,16 @@
 package de.otto.edison.jobs.configuration;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import de.otto.edison.jobs.monitor.JobMonitor;
 import de.otto.edison.jobs.repository.JobRepository;
 import de.otto.edison.jobs.repository.cleanup.KeepLastJobs;
 import de.otto.edison.jobs.repository.cleanup.StopDeadJobs;
 import de.otto.edison.jobs.repository.inmem.InMemJobRepository;
-import de.otto.edison.jobs.repository.mongo.MongoJobRepository;
 import de.otto.edison.jobs.service.DefaultJobService;
 import de.otto.edison.jobs.service.JobService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
