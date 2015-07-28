f873f37c9955b8fdc529862434128c5d70e8bb22
Guido Steinacker <guido.steinacker@otto.de>
2015-07-28 23:10:42 +0200

    added some info + keys to status

--- status/src/main/java/de/otto/edison/status/controller/ApplicationStatusRepresentation.java
+++ status/src/main/java/de/otto/edison/status/controller/ApplicationStatusRepresentation.java
@@ -3,13 +3,28 @@ package de.otto.edison.status.controller;
 import de.otto.edison.status.domain.ApplicationStatus;
 import de.otto.edison.status.domain.StatusDetail;
 import net.jcip.annotations.Immutable;
+import org.springframework.format.datetime.joda.LocalDateTimeParser;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.format.TextStyle;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
+
+import static java.time.ZonedDateTime.now;
+import static java.time.format.DateTimeFormatter.ofLocalizedDateTime;
+import static java.time.format.FormatStyle.LONG;
+import static java.time.format.FormatStyle.MEDIUM;
 
 @Immutable
 final class ApplicationStatusRepresentation {
 
+
+    private static final String SYSTEM_START_TIME = now().format(ofLocalizedDateTime(LONG));
 
     private final ApplicationStatus applicationStatus;
 
@@ -26,6 +41,8 @@ final class ApplicationStatusRepresentation {
             put("status", applicationStatus.getStatus().name());
             put("name", applicationStatus.getName());
             put("hostname", applicationStatus.getHostName());
+            put("systemtime", now().format(ofLocalizedDateTime(LONG)));
+            put("systemstarttime", SYSTEM_START_TIME);
             put("commit", applicationStatus.getVersionInfo().getCommit());
             put("version", applicationStatus.getVersionInfo().getVersion());
             put("statusDetails", statusDetailsRepresentationOf(applicationStatus));
