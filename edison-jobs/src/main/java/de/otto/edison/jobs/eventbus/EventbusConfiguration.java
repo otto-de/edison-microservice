cee375c0283f9431ed8424f6aaadd0aa0d7da8a9
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-12-17 09:43:05 +0100

    configure default event listeners

--- edison-jobs/src/main/java/de/otto/edison/jobs/eventbus/EventbusConfiguration.java
+++ edison-jobs/src/main/java/de/otto/edison/jobs/eventbus/EventbusConfiguration.java
@@ -1,15 +1,27 @@
 package de.otto.edison.jobs.eventbus;
 
+import de.otto.edison.jobs.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static java.time.Clock.systemDefaultZone;
+
 @Configuration
+@ConditionalOnMissingBean(EventbusConfiguration.class)
 public class EventbusConfiguration {
 
+    @Autowired
+    private JobRepository jobRepository;
+
     @Bean
-    @ConditionalOnMissingBean(LogJobEventListener.class)
-    public LogJobEventListener logEventListener() {
+    public JobEventListener logJobEventListener() {
         return new LogJobEventListener();
+    }
+
+    @Bean
+    public JobEventListener persistenceJobEventListener() {
+        return new PersistenceJobEventListener(jobRepository, systemDefaultZone());
     }
 }
