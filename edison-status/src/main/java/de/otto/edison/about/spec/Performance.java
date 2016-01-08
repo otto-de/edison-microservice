b80611e28bf4bdf1e23263a48a7e5993c710a0f6
Guido Steinacker <guido.steinacker@otto.de>
2016-01-08 19:25:22 +0100

    More information about the service and it's dependent services.

--- edison-status/src/main/java/de/otto/edison/about/spec/Performance.java
+++ edison-status/src/main/java/de/otto/edison/about/spec/Performance.java
@@ -9,15 +9,15 @@ import de.otto.edison.annotations.Beta;
 @Beta
 public enum Performance {
     /**
-     * Depending on your overall requirements, this might be something like < 20ms in the 99 percentile:
+     * Depending on your overall requirements, this might be something like &lt; 20ms in the 99 percentile:
      */
     HIGH,
     /**
-     * Depending on your overall requirements, this might be something like < 200ms in the 99 percentile:
+     * Depending on your overall requirements, this might be something like &lt; 200ms in the 99 percentile:
      */
     MEDIUM,
     /**
-     * Depending on your overall requirements, this might be something like < 2000ms in the 99 percentile:
+     * Depending on your overall requirements, this might be something like &lt; 2000ms in the 99 percentile:
      */
     LOW,
     /** Default value if no performance requirements where specified. */
