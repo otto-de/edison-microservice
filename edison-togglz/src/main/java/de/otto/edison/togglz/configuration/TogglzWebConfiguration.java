7c97bc0df058b920711548c6a8e99bcb8b471b64
Jens Schaa <jens.schaa@otto.de>
2016-03-15 16:54:55 +0100

    Revert "Make Togglz compatible to not embedded Tomcat"
    This reverts commit fb8050f0dc814c82eb422d3bbfbb9ed606b4f75e.

--- edison-togglz/src/main/java/de/otto/edison/togglz/configuration/TogglzWebConfiguration.java
+++ edison-togglz/src/main/java/de/otto/edison/togglz/configuration/TogglzWebConfiguration.java
@@ -2,14 +2,24 @@ package de.otto.edison.togglz.configuration;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.togglz.console.TogglzConsoleServlet;
+import org.togglz.servlet.TogglzFilter;
 
 @Configuration
 public class TogglzWebConfiguration {
-    
+
+    @Bean
+    public FilterRegistrationBean togglzFilter() {
+        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(new TogglzFilter());
+        filterRegistration.addUrlPatterns("/*");
+        return filterRegistration;
+    }
+
     @Bean
     public ServletRegistrationBean togglzServlet(@Value("${management.context-path:/internal}") String prefix) {
         return new ServletRegistrationBean(new TogglzConsoleServlet(), prefix + "/togglz/*");
