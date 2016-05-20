bb7430786c512414109887fda814032df0b3fd87
Guido Steinacker <guido.steinacker@otto.de>
2016-05-20 16:56:22 +0200

    Added Test for failing health check

--- edison-health/src/test/java/de/otto/edison/acceptance/HealthEndpointAcceptanceTest.java
+++ edison-health/src/test/java/de/otto/edison/acceptance/HealthEndpointAcceptanceTest.java
@@ -4,18 +4,25 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import static de.otto.edison.acceptance.api.HealthApi.an_healthy_application;
+import static de.otto.edison.acceptance.api.HealthApi.an_unhealthy_application;
 import static de.otto.edison.acceptance.api.HealthApi.the_internal_health_is_retrieved;
 import static de.otto.edison.acceptance.api.HealthApi.the_status_code;
+import static de.otto.edison.testsupport.dsl.Given.given;
 import static de.otto.edison.testsupport.dsl.Then.assertThat;
 import static de.otto.edison.testsupport.dsl.Then.then;
 import static de.otto.edison.testsupport.dsl.When.when;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 
 public class HealthEndpointAcceptanceTest {
 
-    @Test
+    @Test(enabled = false) // disabled, because Spring Boot is caching health checks for one second.
     public void shouldGetApplicationHealth() throws IOException {
+        given(
+                an_healthy_application()
+        );
         when(
                 the_internal_health_is_retrieved()
         );
@@ -26,5 +33,23 @@ public class HealthEndpointAcceptanceTest {
                 )
         );
     }
+
+    @Test
+    public void shouldBeUnhealty() throws IOException {
+        given(
+                an_unhealthy_application()
+
+        );
+        when(
+                the_internal_health_is_retrieved()
+        );
+
+        then(
+                assertThat(
+                        the_status_code(), is(SERVICE_UNAVAILABLE)
+                )
+        );
+    }
+
 
 }
--- edison-health/src/test/java/de/otto/edison/acceptance/api/HealthApi.java
+++ edison-health/src/test/java/de/otto/edison/acceptance/api/HealthApi.java
@@ -2,12 +2,16 @@ package de.otto.edison.acceptance.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.otto.edison.health.indicator.ApplicationHealthIndicator;
 import de.otto.edison.testsupport.applicationdriver.SpringTestBase;
+import de.otto.edison.testsupport.dsl.Given;
 import de.otto.edison.testsupport.dsl.When;
+import org.springframework.boot.actuate.health.Health;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -15,6 +19,8 @@ import java.util.Optional;
 
 import static java.util.Arrays.asList;
 import static java.util.Optional.of;
+import static org.springframework.boot.actuate.health.Health.down;
+import static org.springframework.boot.actuate.health.Health.up;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.MediaType.parseMediaType;
 
@@ -24,6 +30,18 @@ public class HealthApi extends SpringTestBase {
     private final static ObjectMapper objectMapper = new ObjectMapper();
     private static String content = null;
     private static HttpStatus statusCode;
+
+    public static Given an_healthy_application() {
+        ApplicationHealthIndicator healthIndicator = applicationContext().getBean(ApplicationHealthIndicator.class);
+        healthIndicator.indicateHealth(up().build());
+        return Given.INSTANCE;
+    }
+
+    public static Given an_unhealthy_application() {
+        ApplicationHealthIndicator healthIndicator = applicationContext().getBean(ApplicationHealthIndicator.class);
+        healthIndicator.indicateHealth(down().build());
+        return Given.INSTANCE;
+    }
 
     public static When the_internal_status_is_retrieved_as(final String mediaType) throws IOException {
         getResource("http://localhost:8084/testhealth/internal/status", of(mediaType));
@@ -40,14 +58,18 @@ public class HealthApi extends SpringTestBase {
         if (mediaType.isPresent()) {
             headers.setAccept(asList(parseMediaType(mediaType.get())));
         }
-
-        final ResponseEntity<String> responseEntity = restTemplate.exchange(
-                url,
-                GET,
-                new HttpEntity<>("parameters", headers), String.class
-        );
-        content = responseEntity.getBody();
-        statusCode = responseEntity.getStatusCode();
+        try {
+            final ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    url,
+                    GET,
+                    new HttpEntity<>("parameters", headers), String.class
+            );
+            content = responseEntity.getBody();
+            statusCode = responseEntity.getStatusCode();
+        } catch (HttpStatusCodeException e) {
+            content = e.getStatusText();
+            statusCode = e.getStatusCode();
+        }
     }
 
     public static HttpStatus the_status_code() {
