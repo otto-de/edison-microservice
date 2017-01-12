18e29a7d27d2b1b0438e10cccd221b28c37598b9
Guido Steinacker <guido.steinacker@otto.de>
2017-01-12 15:27:48 +0100

    Test coverage

---
+++ edison-core/src/test/java/de/otto/edison/navigation/NavBarConfigurationTest.java
@@ -0,0 +1,46 @@
+package de.otto.edison.navigation;
+
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static de.otto.edison.navigation.NavBarItem.top;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+public class NavBarConfigurationTest {
+
+    private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+    @After
+    public void close() {
+        if (context != null) {
+            context.close();
+        }
+    }
+
+    @Test
+    public void shouldHaveRightNavBar() {
+        context.register(NavBarConfiguration.class);
+        context.refresh();
+
+        final NavBar rightNavBar = context.getBean("rightNavBar", NavBar.class);
+        assertThat(rightNavBar.getItems(), hasSize(1));
+
+        final NavBarItem item = rightNavBar.getItems().get(0);
+        assertThat(item.getLink(), is("/internal/status"));
+        assertThat(item.getTitle(), is("Status"));
+        assertThat(item.getPosition(), is(top()));
+    }
+
+    @Test
+    public void shouldHaveEmptyMainNavBar() {
+        context.register(NavBarConfiguration.class);
+        context.refresh();
+
+        final NavBar mainNavBar = context.getBean("mainNavBar", NavBar.class);
+        assertThat(mainNavBar.getItems(), hasSize(0));
+    }
+}
+
--- edison-togglz/src/test/java/de/otto/edison/togglz/authentication/CredentialsTest.java
+++ edison-togglz/src/test/java/de/otto/edison/togglz/authentication/CredentialsTest.java
@@ -1,6 +1,7 @@
 package de.otto.edison.togglz.authentication;
 
 import org.junit.Test;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.util.Base64Utils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +11,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.util.Base64Utils.encodeToString;
 
 public class CredentialsTest {
 
     @Test
     public void shouldBeAbleToReadCredentialsFromRequest() {
         HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
-        when(httpServletRequest.getHeader("Authorization")).thenReturn("Basic " + Base64Utils.encodeToString("someUsername:somePassword".getBytes()));
+        when(httpServletRequest.getHeader("Authorization"))
+                .thenReturn("Basic " + encodeToString("someUsername:somePassword".getBytes()));
 
         Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
 
