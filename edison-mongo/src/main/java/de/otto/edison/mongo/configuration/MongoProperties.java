20685cfef6393d2c98fae98c61fe9ad7d26b6661
Florian Torkler <florian.torkler@otto.de>
2017-04-12 13:25:18 +0200

    Rename property from `edison.mongo.passwd` to `edison.mongo.password` (to be automatically hidden in environment variables by spring)

--- edison-mongo/src/main/java/de/otto/edison/mongo/configuration/MongoProperties.java
+++ edison-mongo/src/main/java/de/otto/edison/mongo/configuration/MongoProperties.java
@@ -46,7 +46,7 @@ public class MongoProperties {
     /**
      * database user password
      */
-    private String passwd= "";
+    private String password= "";
     /**
      * Represents preferred replica set members to which a query or command can be sent.
      */
@@ -111,12 +111,12 @@ public class MongoProperties {
         this.user = user;
     }
 
-    public String getPasswd() {
-        return passwd;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getReadPreference() {
