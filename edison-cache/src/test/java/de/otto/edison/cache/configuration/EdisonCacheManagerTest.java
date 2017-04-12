9e7843f51a6e445406bc18895e7afaebb7b82ffb
Guido Steinacker <guido.steinacker@otto.de>
2017-04-12 22:16:47 +0200

    Added a possibility to configure CaffeineCache instances as Spring Beans. Added example-cache. Deprecated CacheRegistry. Closes #73

--- edison-cache/src/test/java/de/otto/edison/cache/configuration/EdisonCacheManagerTest.java
+++ edison-cache/src/test/java/de/otto/edison/cache/configuration/EdisonCacheManagerTest.java
@@ -7,16 +7,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
-public class CacheRegistryTest {
+public class EdisonCacheManagerTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldFailOnErrorInSpecification() {
-        new DefaultCacheRegistry(asList(new CaffeineCacheConfig("test", "no_such_property=1")));
+        new EdisonCacheManager(asList(new CaffeineCacheConfig("test", "no_such_property=1")), null);
     }
 
     @Test
     public void shouldConfigureCacheManagerFromRegisteredCaffeineCacheConfigs() {
-        DefaultCacheRegistry cacheRegistry = new DefaultCacheRegistry(asList(new CaffeineCacheConfig("test", "initialCapacity=1")));
+        EdisonCacheManager cacheRegistry = new EdisonCacheManager(
+                asList(new CaffeineCacheConfig("test", "initialCapacity=1")),
+                null
+        );
 
         assertThat(cacheRegistry.getCache("test"), is(notNullValue()));
     }
