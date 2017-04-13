a0b81fbc4450c8e603ca3665688291a982c39f39
Frank Bregulla <frank.bregulla@otto.de>
2017-04-13 14:30:42 +0200

    [edison-core]: Fix MetricsIntegrationTest.

--- edison-core/src/test/java/de/otto/edison/metrics/http/MetricsIntegrationTest.java
+++ edison-core/src/test/java/de/otto/edison/metrics/http/MetricsIntegrationTest.java
@@ -2,36 +2,27 @@ package de.otto.edison.metrics.http;
 
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import de.otto.edison.acceptance.api.StatusApi;
-import de.otto.edison.testsupport.dsl.Then;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static de.otto.edison.acceptance.api.StatusApi.internal_status_is_retrieved_as;
-import static de.otto.edison.acceptance.api.StatusApi.the_returned_content;
-import static de.otto.edison.acceptance.api.StatusApi.the_status_code;
-import static de.otto.edison.testsupport.dsl.Then.then;
-import static de.otto.edison.testsupport.dsl.When.when;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 public class MetricsIntegrationTest {
 
+    private MetricRegistry metricRegistry;
+
+    @Before
+    public void setUp() throws Exception {
+        metricRegistry = StatusApi.applicationContext().getBean(MetricRegistry.class);
+    }
+
     @Test
-    public void shouldReportHttpCountAndTimeToGraphite() throws Exception {
-        MetricRegistry metricRegistry = StatusApi.applicationContext().getBean(MetricRegistry.class);
+    public void shouldReportHttpCountToGraphite() throws Exception {
         long counterBefore = Optional.ofNullable(metricRegistry.getCounters().get("counter.http.get.200")).orElse(new Counter()).getCount();
 
         //when
@@ -40,8 +31,18 @@ public class MetricsIntegrationTest {
         //then
         long counterAfter = metricRegistry.getCounters().get("counter.http.get.200").getCount();
         assertThat(counterAfter - counterBefore).isEqualTo(1);
-        long timerValues = metricRegistry.getTimers().get("timer.http.get").getSnapshot().size();
-        assertThat(timerValues).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReportHttpTimeToGraphite() throws Exception {
+        long timerSnapshotSizeBefore = Optional.ofNullable(metricRegistry.getTimers().get("timer.http.get")).orElse(new Timer()).getSnapshot().size();
+
+        //when
+        internal_status_is_retrieved_as("text/html");
+
+        //then
+        long timerSnapshotSizeAfter = metricRegistry.getTimers().get("timer.http.get").getSnapshot().size();
+        assertThat(timerSnapshotSizeAfter - timerSnapshotSizeBefore).isEqualTo(1);
     }
 
 }
