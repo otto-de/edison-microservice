05dfea518315ae496626eea968f73941649495cb
Henning Strueber <henning.strueber@otto.de>
2018-02-02 14:05:20 +0100

    Fix server error for empty username/password and allow colons in passwords
    Co-authored-by: Jens Schaa <jens.schaa@otto.de>

--- edison-core/src/main/java/de/otto/edison/authentication/Credentials.java
+++ edison-core/src/main/java/de/otto/edison/authentication/Credentials.java
@@ -30,6 +30,7 @@ public class Credentials {
     /**
      * Read username and password from the request's {@code Authorization} header and create a {@code Credentials}
      * object. Requires authorization header to be base64 encoded.
+     *
      * @param request incoming http request
      * @return {@code Optional} with parsed {@code Credentials} if {@code Authorization} header and credentials
      * are present, {@code Optional.empty} otherwise.
@@ -38,10 +39,11 @@ public class Credentials {
         String authorizationHeader = request.getHeader("Authorization");
         if (!StringUtils.isEmpty(authorizationHeader)) {
             String credentials = authorizationHeader.substring(6, authorizationHeader.length());
-            String[] decodedCredentialParts = new String(Base64Utils.decode(credentials.getBytes())).split(":");
-            return Optional.of(new Credentials(decodedCredentialParts[0], decodedCredentialParts[1]));
-        } else {
-            return Optional.empty();
+            String[] decodedCredentialParts = new String(Base64Utils.decode(credentials.getBytes())).split(":", 2);
+            if (!decodedCredentialParts[0].isEmpty() && !decodedCredentialParts[1].isEmpty()) {
+                return Optional.of(new Credentials(decodedCredentialParts[0], decodedCredentialParts[1]));
+            }
         }
+        return Optional.empty();
     }
 }
--- edison-core/src/test/java/de/otto/edison/authentication/CredentialsTest.java
+++ edison-core/src/test/java/de/otto/edison/authentication/CredentialsTest.java
@@ -1,26 +1,42 @@
 package de.otto.edison.authentication;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.util.Base64Utils.encodeToString;
 
 public class CredentialsTest {
 
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    private void mockHttpServletRequestWithAuthentication(String authString) {
+        when(httpServletRequest.getHeader("Authorization"))
+                .thenReturn("Basic " + encodeToString(authString.getBytes()));
+    }
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
     @Test
     public void shouldBeAbleToReadCredentialsFromRequest() {
-        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
-        when(httpServletRequest.getHeader("Authorization"))
-                .thenReturn("Basic " + encodeToString("someUsername:somePassword".getBytes()));
+        // given
+        mockHttpServletRequestWithAuthentication("someUsername:somePassword");
 
+        // when
         Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
 
+        // then
         assertThat(credentials.isPresent(), is(true));
         assertThat(credentials.get().getUsername(), is("someUsername"));
         assertThat(credentials.get().getPassword(), is("somePassword"));
@@ -28,9 +44,48 @@ public class CredentialsTest {
 
     @Test
     public void shouldReturnEmptyCredentialsIfHeaderDoesNotExist() {
-        Optional<Credentials> credentials = Credentials.readFrom(mock(HttpServletRequest.class));
+        // when
+        Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
 
+        // then
         assertThat(credentials.isPresent(), is(false));
     }
 
+    @Test
+    public void shouldReturnEmptyCredentialsIfPasswordNotSet() {
+        // given
+        mockHttpServletRequestWithAuthentication("someUsername:");
+
+        // when
+        Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
+
+        // then
+        assertThat(credentials.isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldReturnEmptyCredentialsIfUsernameNotSet() {
+        // given
+        mockHttpServletRequestWithAuthentication(":password");
+
+        // when
+        Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
+
+        // then
+        assertThat(credentials.isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldReturnCorrectCredentialsIfPasswordContainsColons() {
+        // given
+        mockHttpServletRequestWithAuthentication("user:pass:word");
+
+        // when
+        Optional<Credentials> credentials = Credentials.readFrom(httpServletRequest);
+
+        // then
+        assertThat(credentials.isPresent(), is(true));
+        assertThat(credentials.get().getUsername(), is("user"));
+        assertThat(credentials.get().getPassword(), is("pass:word"));
+    }
 }
