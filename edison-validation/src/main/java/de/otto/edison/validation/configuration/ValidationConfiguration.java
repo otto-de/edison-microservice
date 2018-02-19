22c54fabcecb09d840bbe18796ecc281029b91c9
Simon Weiler <simon.weiler@otto.de>
2018-02-19 15:11:48 +0100

    fix messages for error hal representation
    Co-authored-by: Frank Bregulla <frank.bregulla@otto.de>

--- edison-validation/src/main/java/de/otto/edison/validation/configuration/ValidationConfiguration.java
+++ edison-validation/src/main/java/de/otto/edison/validation/configuration/ValidationConfiguration.java
@@ -1,8 +1,11 @@
 package de.otto.edison.validation.configuration;
 
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class ValidationConfiguration {
@@ -13,6 +16,16 @@ public class ValidationConfiguration {
         source.setBasename("ValidationMessages");
         source.setUseCodeAsDefaultMessage(true);
         return source;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        PlatformResourceBundleLocator resourceBundleLocator =
+                new PlatformResourceBundleLocator(ResourceBundleMessageInterpolator.USER_VALIDATION_MESSAGES, null, true);
+
+        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        factoryBean.setMessageInterpolator(new ResourceBundleMessageInterpolator(resourceBundleLocator));
+        return factoryBean;
     }
 
 }
