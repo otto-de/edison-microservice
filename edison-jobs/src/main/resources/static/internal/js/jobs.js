24e8f6e0a17b7158c3c2d49ea28568da1b03b239
Marco Geweke <marco.geweke@otto.de>
2018-06-19 00:32:56 +0200

    Fix polling urls for jobs page

--- edison-jobs/src/main/resources/static/internal/js/jobs.js
+++ edison-jobs/src/main/resources/static/internal/js/jobs.js
@@ -2,11 +2,14 @@ function update() {
 
     var jobsContainer = $('#jobsContainer');
     var typeFilter = jobsContainer.data("type-filter");
+    if (!typeFilter) {
+        typeFilter='';
+    }
     var jobsUrl = jobsContainer.data("jobs-url");
 
     $.ajax({
         type: "GET",
-        url: jobsUrl + (typeFilter === null ? '' : "?typeFilter=" + typeFilter + "&humanReadable=true"),
+        url: jobsUrl + "?humanReadable=true" + (typeFilter === '' ? '' : "&type=" + typeFilter),
         headers: {
             Accept: "application/json; charset=utf-8",
             "Content-Type": "application/json; charset=utf-8"
@@ -21,8 +24,8 @@ function update() {
             for (var i in data) {
                 var dataRow = null;
                 dataRow = data[i];
-                var jobStatus = $('#job-status-' + dataRow.id);
 
+                var jobStatus = $('#job-status-' + dataRow.id);
                 //There is a new job that is not in this list -> reload page!
                 if (!jobStatus.length) {
                     location.reload();
@@ -50,7 +53,6 @@ function update() {
                 }
                 $("#job-stopped-" + dataRow.id).text(dataRow.stopped);
                 $("#job-runtime-" + dataRow.id).text(dataRow.runtime);
-                //$(".triggerButton").prop('disabled', false);
                 $("#job-last-updated-" + dataRow.id).text(dataRow.lastUpdated);
             }
             setTimeout(update, 4000);
