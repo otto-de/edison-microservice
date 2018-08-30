1632e88ec0cae9d59b8031ef5121bae2acf6799c
Stefan Lier <stefan.lier@otto.de>
2018-08-30 10:05:17 +0200

    Move TogglzTestConfiguration to TestSupport for inclusion in other project

--- edison-testsupport/src/main/java/de/otto/edison/testsupport/togglz/TogglzTestConfiguration.java
+++ edison-testsupport/src/main/java/de/otto/edison/testsupport/togglz/TogglzTestConfiguration.java
@@ -1,4 +1,4 @@
-package de.otto.edison.example;
+package de.otto.edison.testsupport.togglz;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
