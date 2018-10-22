6dad09bf6a885bf61590163289d89d19ee7cfacb
Henning <hstruebe@gmail.com>
2018-10-22 21:34:55 +0200

    Make aws-ssm dependency compileOnly Also make ParamStorePropertySourcePostProcessor depending on existing SsmClient bean

--- edison-core/src/main/java/de/otto/edison/properties/PropertiesConfiguration.java
+++ edison-core/src/main/java/de/otto/edison/properties/PropertiesConfiguration.java
@@ -1,6 +1,7 @@
 package de.otto.edison.properties;
 
 import de.otto.edison.configuration.ParamStoreProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ public class PropertiesConfiguration {
 
     @Bean
     @ConditionalOnProperty(name = "edison.aws.config.paramstore.enabled", havingValue = "true")
+    @ConditionalOnBean(type = "software.amazon.awssdk.services.ssm.SsmClient")
     public ParamStorePropertySourcePostProcessor paramStorePropertySourcePostProcessor(final SsmClient ssmClient){
         return new ParamStorePropertySourcePostProcessor(ssmClient);
     }
