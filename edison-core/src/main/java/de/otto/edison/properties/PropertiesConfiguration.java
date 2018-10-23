c7cb02c57e089b736da3817d84761156ead4edac
Henning Strueber <hstruebe@gmail.com>
2018-10-23 09:11:38 +0200

    Move conditional annotations for PropertiesConfiguration to class level and add property for edison-job example

--- edison-core/src/main/java/de/otto/edison/properties/PropertiesConfiguration.java
+++ edison-core/src/main/java/de/otto/edison/properties/PropertiesConfiguration.java
@@ -1,20 +1,18 @@
 package de.otto.edison.properties;
 
-import de.otto.edison.configuration.ParamStoreProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.ssm.SsmClient;
 
 @Configuration
+@ConditionalOnProperty(name = "edison.aws.config.paramstore.enabled", havingValue = "true")
+@ConditionalOnBean(type = "software.amazon.awssdk.services.ssm.SsmClient")
 public class PropertiesConfiguration {
 
     @Bean
-    @ConditionalOnProperty(name = "edison.aws.config.paramstore.enabled", havingValue = "true")
-    @ConditionalOnBean(type = "software.amazon.awssdk.services.ssm.SsmClient")
-    public ParamStorePropertySourcePostProcessor paramStorePropertySourcePostProcessor(final SsmClient ssmClient){
+    public ParamStorePropertySourcePostProcessor paramStorePropertySourcePostProcessor(final SsmClient ssmClient) {
         return new ParamStorePropertySourcePostProcessor(ssmClient);
     }
 }
