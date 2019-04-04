3544cdf39360a083201fec5c8258ae9c5818d1e6
Jan Schawo <jan.schawo@otto.de>
2019-04-04 15:34:08 +0200

    add TYPE_USE to @SafeId validation annotation
    Co-authored-by: Florian Torkler <Florian.Torkler@otto.de>

--- edison-validation/src/main/java/de/otto/edison/validation/validators/SafeId.java
+++ edison-validation/src/main/java/de/otto/edison/validation/validators/SafeId.java
@@ -3,14 +3,15 @@ package de.otto.edison.validation.validators;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.*;
+
 @Documented
 @Constraint(validatedBy = {SafeIdValidator.class})
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target({METHOD, FIELD, PARAMETER, TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SafeId {
 
