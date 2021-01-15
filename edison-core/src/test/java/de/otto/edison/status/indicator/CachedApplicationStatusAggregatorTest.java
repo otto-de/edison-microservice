4bd7b4a7cbc9ce052451d2d2d49abd9f70c3232e
Frank Bregulla <frank.bregulla@otto.de>
2021-01-15 10:03:28 +0100

    catch exception in status detail aggregator so that processing of other detail aggregators is not aborted
    Co-authored-by: Andrada Korablow <andrada.korablow@otto.de>
    Co-authored-by: Matthias Geißendörfer <matthias.geissendoerfer@otto.de>

--- edison-core/src/test/java/de/otto/edison/status/indicator/CachedApplicationStatusAggregatorTest.java
+++ edison-core/src/test/java/de/otto/edison/status/indicator/CachedApplicationStatusAggregatorTest.java
@@ -5,15 +5,14 @@ import de.otto.edison.status.domain.Status;
 import de.otto.edison.status.domain.StatusDetail;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static de.otto.edison.status.domain.StatusDetail.statusDetail;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CachedApplicationStatusAggregatorTest {
 
@@ -21,9 +20,10 @@ public class CachedApplicationStatusAggregatorTest {
     public static final StatusDetail OK_DETAIL_TWO = statusDetail("two", Status.OK, "a message");
     public static final StatusDetail WARNING_DETAIL = statusDetail("iHaveAWarning", Status.WARNING, "a message");
     public static final StatusDetail ERROR_DETAIL = statusDetail("thatsAnError", Status.ERROR, "a message");
+    public static final StatusDetail EXCEPTION_DETAIL = statusDetail("ErrorThrowingStatusDetailIndicator", Status.ERROR, "got exception: boom");
 
     @Test
-    public void shouldCacheStatus() throws Exception {
+    public void shouldCacheStatus() {
         // given
         final StatusDetailIndicator mockIndicator = someStatusDetailIndicator(OK_DETAIL_ONE);
         final ApplicationStatusAggregator statusAggregator = new CachedApplicationStatusAggregator(
@@ -39,13 +39,13 @@ public class CachedApplicationStatusAggregatorTest {
     }
 
     @Test
-    public void shouldAggregateStatusDetails() throws Exception {
+    public void shouldAggregateStatusDetails() {
         // given
         final ApplicationStatusAggregator statusAggregator = new CachedApplicationStatusAggregator(
                 mock(ApplicationStatus.class), asList(
-                        someStatusDetailIndicator(OK_DETAIL_ONE),
-                        someStatusDetailIndicator(ERROR_DETAIL)
-                )
+                someStatusDetailIndicator(OK_DETAIL_ONE),
+                someStatusDetailIndicator(ERROR_DETAIL)
+        )
         );
         statusAggregator.update();
         // when
@@ -57,13 +57,31 @@ public class CachedApplicationStatusAggregatorTest {
     }
 
     @Test
-    public void shouldAggregateCompositeStatusDetails() throws Exception {
+    public void shouldCatchExceptionsInStatusDetails() {
         // given
         final ApplicationStatusAggregator statusAggregator = new CachedApplicationStatusAggregator(
                 mock(ApplicationStatus.class), asList(
-                        someCompositeStatusDetailIndicator(OK_DETAIL_ONE, WARNING_DETAIL),
-                        someStatusDetailIndicator(OK_DETAIL_TWO)
-                )
+                new ErrorThrowingStatusDetailIndicator(),
+                someStatusDetailIndicator(OK_DETAIL_TWO)
+        )
+        );
+        statusAggregator.update();
+        // when
+        statusAggregator.aggregatedStatus();
+        // then
+        assertThat(statusAggregator.aggregatedStatus().status, is(Status.ERROR));
+        assertThat(statusAggregator.aggregatedStatus().statusDetails.get(0), is(EXCEPTION_DETAIL));
+        assertThat(statusAggregator.aggregatedStatus().statusDetails.get(1), is(OK_DETAIL_TWO));
+    }
+
+    @Test
+    public void shouldAggregateCompositeStatusDetails() {
+        // given
+        final ApplicationStatusAggregator statusAggregator = new CachedApplicationStatusAggregator(
+                mock(ApplicationStatus.class), asList(
+                someCompositeStatusDetailIndicator(OK_DETAIL_ONE, WARNING_DETAIL),
+                someStatusDetailIndicator(OK_DETAIL_TWO)
+        )
         );
         statusAggregator.update();
         // when
@@ -85,5 +103,14 @@ public class CachedApplicationStatusAggregatorTest {
         final StatusDetailIndicator mockIndicator = mock(StatusDetailIndicator.class);
         when(mockIndicator.statusDetails()).thenReturn(asList(statusDetail));
         return mockIndicator;
+    }
+
+
+    class ErrorThrowingStatusDetailIndicator implements StatusDetailIndicator {
+
+        @Override
+        public List<StatusDetail> statusDetails() {
+            throw new RuntimeException("boom");
+        }
     }
 }
