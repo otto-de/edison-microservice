be590ad4abc360bac194b4878499a989d9fa1d07
fouquetp <peter.fouquet@ottogroup.com>
2021-04-28 11:45:19 +0200

    [edison-togglz]** use ConditionalOnClass, because sometimes the mongoClient was not created before the MongoTogglzConfiguration and the bean was missing

--- edison-togglz/src/main/java/de/otto/edison/togglz/configuration/MongoTogglzConfiguration.java
+++ edison-togglz/src/main/java/de/otto/edison/togglz/configuration/MongoTogglzConfiguration.java
@@ -1,5 +1,6 @@
 package de.otto.edison.togglz.configuration;
 
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import de.otto.edison.mongo.configuration.MongoConfiguration;
 import de.otto.edison.mongo.configuration.MongoProperties;
@@ -8,6 +9,7 @@ import de.otto.edison.togglz.mongo.MongoTogglzRepository;
 import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +21,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Configuration
 @AutoConfigureAfter(MongoConfiguration.class)
 @ConditionalOnProperty(prefix = "edison.togglz", name = "mongo.enabled", havingValue = "true", matchIfMissing = true)
-@ConditionalOnBean(type = "com.mongodb.client.MongoClient")
+@ConditionalOnClass(MongoClient.class)
 public class MongoTogglzConfiguration {
 
     private static final Logger LOG = getLogger(MongoTogglzConfiguration.class);
