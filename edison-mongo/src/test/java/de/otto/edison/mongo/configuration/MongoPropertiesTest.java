3c0be205db30bdb6cce6733043698829a1ddaa38
Dirk Ehms <dirk.ehms@otto.de>
2022-03-24 14:25:06 +0100

    Add support for MongoDB ConnectionString (AWS_IAM)

--- edison-mongo/src/test/java/de/otto/edison/mongo/configuration/MongoPropertiesTest.java
+++ edison-mongo/src/test/java/de/otto/edison/mongo/configuration/MongoPropertiesTest.java
@@ -1,5 +1,6 @@
 package de.otto.edison.mongo.configuration;
 
+import com.mongodb.AuthenticationMechanism;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCompressor;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class MongoPropertiesTest {
 
@@ -42,5 +44,38 @@ public class MongoPropertiesTest {
 
         //then
         assertThat(mongoClientOptions.getCompressorList(), is(Collections.emptyList()));
+    }
+
+    @Test
+    void shouldUseConnectionStringWhenUriPatternWithAwsIamIsProvided() {
+        //given
+        final MongoProperties props = new MongoProperties();
+        props.setUriPattern("mongodb+srv://cluster0.x4oml.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-AWS");
+
+        //when
+        MongoClientSettings mongoClientOptions = props.toMongoClientSettings(MongoClientSettings.getDefaultCodecRegistry(), null, null);
+
+        //then
+        assertThat(mongoClientOptions.getCredential(), is(notNullValue()));
+        assertThat(mongoClientOptions.getCredential().getAuthenticationMechanism(), is(AuthenticationMechanism.MONGODB_AWS));
+        assertThat(mongoClientOptions.getCredential().getSource(), is("$external"));
+    }
+
+    @Test
+    void shouldUseConnectionStringWhenUriPatternWithScramIsProvided() {
+        //given
+        final MongoProperties props = new MongoProperties();
+        props.setUser("aUsername");
+        props.setPassword("somePassword");
+        props.setUriPattern("mongodb+srv://%s:%s@cluster0.x4oml.mongodb.net/myFirstDatabase");
+
+        //when
+        MongoClientSettings mongoClientOptions = props.toMongoClientSettings(MongoClientSettings.getDefaultCodecRegistry(), null, null);
+
+        //then
+        assertThat(mongoClientOptions.getCredential(), is(notNullValue()));
+        assertThat(mongoClientOptions.getCredential().getUserName(), is("aUsername"));
+        assertThat(mongoClientOptions.getCredential().getPassword(), is("somePassword".toCharArray()));
+        assertThat(mongoClientOptions.getCredential().getSource(), is("admin"));
     }
 }
