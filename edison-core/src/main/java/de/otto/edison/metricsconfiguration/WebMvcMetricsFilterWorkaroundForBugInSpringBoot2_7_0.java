41c8a0c865be316410c1258d52e0a9f08789a2f3
MediaMarco <marco.geweke@otto.de>
2022-05-24 21:44:45 +0200

    Change condition to ConditionalOnMissingFilterBean
    Co-authored-by: Marco Geweke <marco.geweke@otto.de>

--- edison-core/src/main/java/de/otto/edison/metricsconfiguration/WebMvcMetricsFilterWorkaroundForBugInSpringBoot2_7_0.java
+++ edison-core/src/main/java/de/otto/edison/metricsconfiguration/WebMvcMetricsFilterWorkaroundForBugInSpringBoot2_7_0.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
 import org.springframework.boot.actuate.metrics.web.servlet.WebMvcMetricsFilter;
 import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTagsProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
@@ -12,12 +13,15 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.DispatcherType;
 
-/* TODO: Remove this when Spring Boot 2.7.1 is there */
+/*
+   TODO: Remove this class when Spring Boot 2.7.1 is there
+   https://github.com/spring-projects/spring-boot/issues/31150
+ */
 @Component
 public class WebMvcMetricsFilterWorkaroundForBugInSpringBoot2_7_0 {
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingFilterBean
     public FilterRegistrationBean<WebMvcMetricsFilter> webMvcMetricsFilter(MetricsProperties properties,
                                                                            MeterRegistry registry, WebMvcTagsProvider tagsProvider) {
         MetricsProperties.Web.Server.ServerRequest request = properties.getWeb().getServer().getRequest();
