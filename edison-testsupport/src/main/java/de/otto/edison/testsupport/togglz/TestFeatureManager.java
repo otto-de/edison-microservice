6eb280bbdfae7ebcf5e146ae6f400df7c3a14b2c
Marco Geweke <marco.geweke@gmail.com>
2023-02-09 09:56:09 +0100

    Fix enableAll in TestFeatureManager #193

--- edison-testsupport/src/main/java/de/otto/edison/testsupport/togglz/TestFeatureManager.java
+++ edison-testsupport/src/main/java/de/otto/edison/testsupport/togglz/TestFeatureManager.java
@@ -37,7 +37,7 @@ public class TestFeatureManager implements FeatureManager {
 
     @Override
     public Set<Feature> getFeatures() {
-        return new HashSet<Feature>(Arrays.asList(featureEnum.getEnumConstants()));
+        return new HashSet<>(Arrays.asList(featureEnum.getEnumConstants()));
     }
 
     @Override
@@ -99,7 +99,7 @@ public class TestFeatureManager implements FeatureManager {
 
     public TestFeatureManager enableAll() {
         for (FeatureState featureState : featureStates.values()) {
-            featureState.setEnabled(false);
+            featureState.setEnabled(true);
         }
         return this;
     }
